f45e39e1c1e637f5960e630be9d332d365438e7f
DomTripodi93 <dtripodi93@gmail.com>
2020-10-04 21:36:35 -0400

    added invoiceRoutes

---
+++ backend/routes/invoiceRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const checkAuth = require("../middleware/checkAuth");
+
+function routes(Invoice){
+    const router = express.Router();
+    const controller = InvoiceController(Invoice);
+
+    router.route("")
+        .post(checkAuth, controller.post);
+
+    router.route("/byNumber/:invoiceNumber")
+        .get(checkAuth, controller.getByNumber);
+
+    router.route("/byDateRange/:startDate&:endDate")
+        .get(checkAuth, controller.getByPeriod);
+    
+    router.route("/:_id")
+        .put(checkAuth, controller.put)
+        .delete(checkAuth, controller.deleteInvoice);
+
+    return router;
+}
+
+module.exports = routes;
