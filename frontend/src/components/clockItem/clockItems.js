a0002cd68d3b9bb347a91614078370f57ba263ac
DomTripodi93 <dtripodi93@gmail.com>
2020-10-07 17:42:15 -0400

    added single clock item component

---
+++ frontend/src/components/clockItem/clockItems.js
@@ -0,0 +1,37 @@
+import React from 'react';
+// import SingleClockItem from './single-clock-item';
+
+const ClockItems = props => {
+    // <SingleClockItem
+    //     clockItem={clockItem} />
+    return (
+        <div>
+            {props.clockItems.length > 0 ?
+                <div>
+                    <div className='grid50-to-100'>
+                        {props.clockItems.map(clockItem => (
+                            <div
+                                key={clockItem._id}
+                            >
+                                {clockItem._id}
+                                <br />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                :
+                <div className="border centered">
+                    <h4 className="spaced">
+                        You currently don't have any clock items!
+                    </h4>
+                    <h4 className="spaced">
+                        Add some clock items using the button above to see them here.
+                    </h4>
+                </div>
+            }
+        </div>
+    )
+}
+
+
+export default ClockItems;
